962c11837bb44be9169708cf10df2d9d116d18d9
Blox <40967965+BloxOfficial@users.noreply.github.com>
2019-11-18 12:26:39 -0600

    Centinelas by Cr1mmson
    Centinelas is a note giver for school teachers

---
+++ C++/Centinelas by Cr1mmson.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/*const : antecede al tipo de dato que queremos crear y hace constante y no se podra modificar
+el valor de la variable y en dado caso que se modifique el valor de la varialble el compilador
+mandara un erro */
+
+int main(){
+    const int centinela = -1;
+    int nota;
+
+    printf("introdusca primer nota : ");
+    scanf("%d",&nota);
+    while(nota != centinela){
+        printf("introdusca la siguiente nota : ");
+        scanf("%d",&nota);
+            break;
+    }
+    return 0;
+}
